71f23b44e6128b76d9ef9cafd5c8cee75986b006
Chirag Kushwaha <chirag.kushwaha@eka.care>
2023-05-13 15:42:03 +0530

    deployment setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
@@ -11,19 +11,19 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDTDCPYikRdOHWxGceiRbtjjxdWJ7oyKiY",
-  authDomain: "b00by-trap.firebaseapp.com",
-  projectId: "b00by-trap",
-  storageBucket: "b00by-trap.appspot.com",
-  messagingSenderId: "846423423070",
-  appId: "1:846423423070:web:41727b077cecd37b295ccf",
-  measurementId: "G-35VMBZ96VK",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTDCPYikRdOHWxGceiRbtjjxdWJ7oyKiY",
+//   authDomain: "b00by-trap.firebaseapp.com",
+//   projectId: "b00by-trap",
+//   storageBucket: "b00by-trap.appspot.com",
+//   messagingSenderId: "846423423070",
+//   appId: "1:846423423070:web:41727b077cecd37b295ccf",
+//   measurementId: "G-35VMBZ96VK",
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
